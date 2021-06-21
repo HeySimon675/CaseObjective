413f6fb8fed3e3a55fb2fbac4d2247e76d37e509
Simão <gabriel.simao@topi.io>
2021-06-21 01:00:31 -0300

    Removing cancel option in AskMealMessage | CreateMealMessage returns null when the cancel option is selected, as in the example game

--- src/br/com/simao/jogoGourmet/view/AskMealMessage.java
+++ src/br/com/simao/jogoGourmet/view/AskMealMessage.java
@@ -5,10 +5,11 @@ import javax.swing.*;
 public class AskMealMessage {
 
     private static final String ASK_MESSAGE = "O prato que você pensou é %s?";
+    private static final String CONFIRM_MESSAGE = "Responda";
 
     public static Boolean ask(String message) {
         message = String.format(ASK_MESSAGE, message);
-        int response = JOptionPane.showConfirmDialog(null, message);
+        int response = JOptionPane.showConfirmDialog(null, message,CONFIRM_MESSAGE,JOptionPane.YES_NO_OPTION);
         return response == 0;
     }
 
